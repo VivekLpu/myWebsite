63afb69873e123f542595fc6010a5f56395b8859
Vivek Kumar <vivek.kumar@lpu.in>
2024-10-10 14:37:51 +0530

    Some more concepts are there

---
+++ mygreeting.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greetings</title>
+</head>
+<body>
+    <?php
+    if (isset($_COOKIE["preflang"])) {
+        $user_lang = $_COOKIE["preflang"];
+        
+        if ($user_lang == "eng") {
+            echo "Welcome!!";
+        } elseif ($user_lang == "french") {
+            echo "Bonjour!!";
+        } elseif ($user_lang == "malayalam") {
+            echo "Namaste!!";
+        }
+    } else {
+        echo "No language preference set!";
+    }
+    ?>
+</body>
+</html>
--- userLanguage.php
+++ userLanguage.php
@@ -6,23 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-        Select the Language:
+    <form method="post" action="">
+        Select the Language: 
         <select name="lang">
             <option value="eng">English</option>
-            <option value="french">French</option>
-            <option value="malyalam">Malyalam</option>
+            <option value="french">Malayalam</option>
+            <option value="malayalam">French</option>
         </select>
-        <input type="submit" value="submit" value="SET LANGUAGE">
+        <input type="submit" value="SET LANGUAGE"><form>
     </form>
-
-    <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if(isset($_POST["submit"])){
-            $pref_lang=$_POST["lang"];
-        }
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(isset($_POST["submit"])){
+        $pref_lang=$_POST["lang"];
+        setcookie("preflag",$pref_lang,time()+(7*86400)+"/");
+        header('Location: mygreetings.php');
+        exit;
     }
-    
-    ?>
+}
+?>
 </body>
 </html>
